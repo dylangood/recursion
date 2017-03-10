ac8d30d9a455dfa400e081493a2b2212a8dcc419
Dylan Gould <dylangood@gmail.com>
2017-03-10 12:12:34 -0800

    Write pseudocode plus first few easy cases
    I mostly just wrote pseudocode about the process - my thinking still
    seems a little bit muddled, but there are some easy cases that can be
    detected early and dealt with, so I started with those. In the case
    of returning undefined vs. null, even the easy cases turned out to be
    a little bit harder than I expected.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,23 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+	// If something is undefined or a function, skip it - return (undefined)
+	if( jQuery.isFunction(obj) || undefined === obj ) {
+		return;
+	}
+	  //Unfortunately, when called recursively, the above 2 results should apparently be 'null'
+	  //NOTE: I shall have to come back and fix this later!!!
+
+	// If something is a number, null, boolean, or string, then don't recurse
+	  // Instead, choose the correct option and stringify it
+	  // Return the string
+	if( jQuery.isNumeric(obj) ) {
+		return obj.toString();
+	}
+	// If it's a collection, add an opening for the correct type of collection
+	  // If it's a non-array object, check whether it's empty
+	    // If empty, add a closing }
+	    // Otherwise add the next key, and recurse on next value, and then add a closing }
+	  // If it's an array, add an opening [, recurse on each value, and then add a closing ]
+    // If there are more elements left, add a comma+space
 };
