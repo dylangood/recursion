14262675d9c16404768a2a4483345f9537a0cab4
Dylan Gould <dylangood@gmail.com>
2017-03-10 16:22:33 -0800

    Implement remainder of non-collection objects & attempt recursive
    So this code includes the tests and return values of the rest of the possible basic
    types of objects - the ones that aren't a collection themselves. I also took a stab
    at wrapping the function in a special recursive function call that will return the
    string 'null' for undefined results, since that is how JSON.stringify appears to
    behave.
    
    Next steps: begin working on collections and recursive calls.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,23 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+	
 	// If something is undefined or a function, skip it - return (undefined)
-	if( jQuery.isFunction(obj) || undefined === obj ) {
-		return;
+	if( 'function' === $.type(obj) || 'undefined' === $.type(obj) ) {
+		return undefined;
 	}
 	  //Unfortunately, when called recursively, the above 2 results should apparently be 'null'
 	  //NOTE: I shall have to come back and fix this later!!!
-
-	// If something is a number, null, boolean, or string, then don't recurse
+    var recursiveStringifyJSON = function(obj) {
+      var result = stringifyJSON(obj);
+      if( 'undefined' === $.type(result) ) {
+      	return 'null';
+      } else {
+      	return result;
+      }
+    }
+	// If element is a number, null, boolean, or string, then don't recurse
 	  // Instead, choose the correct option and stringify it
 	  // Return the string
-	if( jQuery.isNumeric(obj) ) {
+	if( jQuery.isNumeric(obj) || 'boolean' === $.type(obj) ) {
 		return obj.toString();
+	}
+
+	if ( 'null' === $.type(obj) ) {
+		return 'null';
+	}
+
+	if( 'string' === $.type(obj) ) {
+		return ('"' + obj + '"');
 	}
 	// If it's a collection, add an opening for the correct type of collection
 	  // If it's a non-array object, check whether it's empty
 	    // If empty, add a closing }
 	    // Otherwise add the next key, and recurse on next value, and then add a closing }
 	  // If it's an array, add an opening [, recurse on each value, and then add a closing ]
-    // If there are more elements left, add a comma+space
+    // If there are more elements left, add a comma
 };
