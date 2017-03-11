864c550b77157b14ba42abb3ef39d0911fecc613
Dylan Gould <dylangood@gmail.com>
2017-03-10 18:36:26 -0800

    Magically solves the comma problem and finishes collections
    I still don't actually know where the commas are coming from, because I didn't
    code them into the algorithm. But something is making them, so I guess...
    THANKS, SOMETHING!
    
    Collections are solved, both types. All tests pass.
    
    And also, even though my solution and the target don't fully agree on what
    to do in undefined cases, I found that I didn't need to special-case the
    output, so the "recursiveStringifyJSON" function became unnecessary.
    I did have to do a little tweaking to get the proper behavior of skipping
    the entire property into the object clause for those cases. Wish I had
    captured some of that in merges, just in case, because it's interesting.
    Oh well!

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,39 +4,51 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+
+	var objType = $.type(obj);
 	
 	// If something is undefined or a function, skip it - return (undefined)
-	if( 'function' === $.type(obj) || 'undefined' === $.type(obj) ) {
-		return undefined;
+	if( 'function' === objType || 'undefined' === objType ) {
+		return;
 	}
-	  //Unfortunately, when called recursively, the above 2 results should apparently be 'null'
-	  //NOTE: I shall have to come back and fix this later!!!
-    var recursiveStringifyJSON = function(obj) {
-      var result = stringifyJSON(obj);
-      if( 'undefined' === $.type(result) ) {
-      	return 'null';
-      } else {
-      	return result;
-      }
-    }
+
 	// If element is a number, null, boolean, or string, then don't recurse
 	  // Instead, choose the correct option and stringify it
 	  // Return the string
-	if( jQuery.isNumeric(obj) || 'boolean' === $.type(obj) ) {
+	if( 'number' === objType || 'boolean' === objType ) {
 		return obj.toString();
 	}
 
-	if ( 'null' === $.type(obj) ) {
+	if ( 'null' === objType ) {
 		return 'null';
 	}
 
-	if( 'string' === $.type(obj) ) {
+	if( 'string' === objType ) {
 		return ('"' + obj + '"');
 	}
+
 	// If it's a collection, add an opening for the correct type of collection
-	  // If it's a non-array object, check whether it's empty
-	    // If empty, add a closing }
-	    // Otherwise add the next key, and recurse on next value, and then add a closing }
+	if( 'array' === objType ) {
+		if( jQuery.isEmptyObject(obj) ) {
+			return '[]';
+		}
 	  // If it's an array, add an opening [, recurse on each value, and then add a closing ]
+	  return ('[' + jQuery.map(obj, stringifyJSON) + ']' );
+	}
+
+	// If it's a non-array object, check whether it's empty
+	if( 'object' === objType ) {
+      if( jQuery.isEmptyObject(obj) ) {
+      	return '{}';
+      }
+      return ('{' + jQuery.map(obj, function(elem, i){ 
+      	if( 'undefined' !== $.type( stringifyJSON(elem) ) ) {
+      		return ('"' + i + '":' + stringifyJSON(elem) ); 
+      	} 	
+      }) + '}');
+	}
+	    // Otherwise add the next key, and recurse on next value, and then add a closing }
+
     // If there are more elements left, add a comma
-};
+    // NOTE TO SELF: For some reason this step turned out to be unnecessary. Why not? Where are the commas coming from?
+}
