2ac1cc496d910257d2ab31475efab311dbca9b09
Dylan Gould <dylangood@gmail.com>
2017-03-14 15:50:59 -0700

    First attempt using actual code
    This isn't great - it doesn't even compile yet. But since I found some instructions
    about some interface properties & methods that are available for me to use in this
    exercise, it doesn't look quite so complicated now. Still, I'm having trouble
    getting console to spit out the result HTMLCollection that I need to replicate.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,14 +6,36 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
+	var result = new HTMLCollection();
+	var focusElement = this; // <-- I STILL HAVE A BIG PROBLEM HERE!!!
+
+	for( i = 0; i > focusElement.childNodes.length; i++ ) {
+
+		currentElement = focusElement.childNodes[i];
+
+		if( currentElement.classList.contains(className) ) {
+			result.push(currentElement);
+		}
+
+		// call getElementsByClassName, with currentElement as the focusElement
+		getElementsByClassName.call(currentElement, className);
+	}
+
+	
+
   // 1. Find all the elements on the current level that have 'class="className"'
      //  a. Define the edges of the "current level"
      //  b. Between those edges, iterate through each element
      //  c. Check each one to see if it has className, keeping in mind that there may be other classes
   // 2. Push them onto a results array
-     //  a. Research: Do the arrayed copies have to leave out sub-nodes or include them?
-     //  b. Research: Or are they supposed to return the entire collection, filtering out any nodes that lack the class?
+     //  a. Research: Do the arrayed copies have to leave out child nodes or include them?
+     //  b. Research: Or is this function supposed to return the entire collection, filtering out any nodes that lack the class?
      //  c. Research: When I'm testing the original function in the browser, these objects are really deep!!! This seems like a lot of work for one exercise.
   // 3. For the ones that don't have the class, recursively call the function on their content.
      //  a. Exactly how to do this depends a lot on the answers to 2a and 2b, above.
+
+  // As always, RTFI:
+     // "Use document.body, element.childNodes, and element.classList"
+
+     return result;
 };
